48251f5984a67c6b9b943f8c75d26f25a89864a0
josemariojrs <91684287+josemariojrs@users.noreply.github.com>
2021-10-06 15:52:05 -0300

    Add files via upload

---
+++ Projeto tractian2.cpp
@@ -0,0 +1,30 @@
+#include <conio.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main () {
+ int length;
+ char * buffer;
+
+ ifstream is;
+ is.open ("1602245833-2715-NAO7856.txt", ios::in);
+
+ is.seekg (0, ios::end);
+ length = is.tellg();
+ is.seekg (0, ios::beg);
+
+ if (is.is_open())
+ {
+    int n = 0;
+    buffer = new char[length];
+    is.read(buffer,length);
+
+ }
+ else {
+   printf("Arquivo não foi aberto");
+   getch();
+}
+
+  is.close();
+}
